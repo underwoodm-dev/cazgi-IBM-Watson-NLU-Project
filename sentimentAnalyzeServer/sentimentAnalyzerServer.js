2dea2ffcafe350ebd5ab386f5b0cb29b7cd539e0
underwoodm-dev <83571955+underwoodm-dev@users.noreply.github.com>
2021-08-20 19:12:12 -0400

    Update sentimentAnalyzerServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = new express();
 
 app.use(express.static('client'))
@@ -31,3 +33,20 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const NaturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1 ({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator ({
+            apikey:api_key,
+        }),
+        serviceUrl:api_url,
+    });
+
+    return NaturalLanguageUnderstanding;
+}
